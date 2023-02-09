0e9494131ea20eadacbbe2f05c972c1953a6a032
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2023-02-09 12:59:57 +0300

    add form example

--- server/api/send.post.js
+++ server/api/send.post.js
@@ -2,6 +2,22 @@ import nodemailer from 'nodemailer';
 
 export default defineEventHandler(async (event) => {
 
+    const body = await readBody(event)
+
+      let htmlBody = `<table style="width: 100%;">`;
+
+    for (const key in body) {
+        if (Object.hasOwnProperty.call(body, key) && key !== 'files') {
+            htmlBody =
+                htmlBody +
+                `<tr style="background-color: #f8f8f8;">
+              <td style="padding: 10px; border: #e9e9e9 1px solid;"><b>${key}</b></td>
+              <td style="padding: 10px; border: #e9e9e9 1px solid;">${body[key]}</td>
+              </tr>`;
+        }
+    }
+    htmlBody = htmlBody + `</table>`;
+
     const transporter = nodemailer.createTransport({
         port: 465, // true for 465, false for other ports
         host: '',
@@ -17,6 +33,7 @@ export default defineEventHandler(async (event) => {
         to: '',
         subject: 'Hello world',
         text: `Hello`,
+        html: htmlBody,
         attachments: []
     };
 
